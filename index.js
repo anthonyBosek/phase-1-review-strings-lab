b08e9405e868d25e2e087f1d770638739d1fef69
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-09 10:03:57 -0700

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Leo Messi";
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = `Welcome, ${currentUser[0]}!`;
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
